53756a6059ad5241c0504538974e393ad7216b46
Eddie Bowen <eddie@cosmo7.com>
2022-05-21 13:34:06 -0400

    Initial commit

---
+++ Observable.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+
+
+public class Observable<T>
+{
+	private T _value;
+	private Action<T> callbacks;
+
+	public Observable()
+	{
+
+	}
+
+	public Observable(T initialValue)
+	{
+		_value = initialValue;
+	}
+
+	public void Set(T value)
+	{
+		if (!object.Equals(_value, value))
+		{
+			_value = value;
+			callbacks.Invoke(value);
+		}
+	}
+
+	public T Get()
+	{
+		return _value;
+	}
+
+	public T value
+	{
+		get
+		{
+			return _value;
+		}
+
+		set
+		{
+			Set(value);
+		}
+	}
+
+	public void Bind(Action<T> call)
+	{
+		callbacks += call;
+	}
+
+	public void Unbind(Action<T> call)
+	{
+		callbacks -= call;
+	}
+}
+
